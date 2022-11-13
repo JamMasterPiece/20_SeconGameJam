5991e35001822a5fc7f3c0bd308829e9d1879552
Güven Sarı <59062942+SezginGuvenSari@users.noreply.github.com>
2022-11-13 13:17:10 +0300

    Import platform sprites and integration background

---
+++ Assets/Scripts/InGameScripts/Player/PlayerEffects.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Unity.VisualScripting;
+
+public class PlayerEffects : MonoBehaviour
+{
+    #region Serialize
+
+    
+
+    #endregion
+
+    //Some effects code.
+}
